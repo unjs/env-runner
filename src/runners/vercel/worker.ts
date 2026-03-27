6c469b394eb2332f732e41ad4ad3b87b6f1a5138
Rihan Arfan <me@file.properties>
2026-03-27 14:12:05 -0700

    fix: relative import

--- src/runners/vercel/worker.ts
+++ src/runners/vercel/worker.ts
@@ -1,4 +1,4 @@
 const SYMBOL_FOR_REQ_CONTEXT = Symbol.for("@vercel/request-context");
 (globalThis as any)[SYMBOL_FOR_REQ_CONTEXT] = { get: () => ({}) };
 
-await import("env-runner/runners/node-worker/worker");
+await import("../node-worker/worker.ts");
